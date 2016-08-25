b5b0a17d02f03ac0ba788b1e143b6cb1fa313151
Richard Purves <contact@richard-purves.com>
2016-08-25 13:12:58 +0200

    Reworked
    Changed to add extra localisation but only for specific countries.

--- LanguageSelect.sh
+++ LanguageSelect.sh
@@ -1,156 +1,127 @@
 #!/bin/bash
 
 # Script to set the system default language
+# I'd use profiles if I wanted to lock these settings down, but I only wish to set them once. Hence this script.
 
 # Author: Richard Purves <richard at richard - purves dot com>
 # Version 1.0 : 25-08-2016 - Initial version
+# Version 1.1 : 25-08-2016 - reworked for specific countries plus other localisation settings.
 
-# Set up working variables from info passed to the script
-language=$4
+country=$4
 
-case $language in
+case $country in
 
-	english)
-		languagesetup -langspec 1
-	;;
+ch)
+	langspec="3"
+	lang="de"
+	keyboard="Swiss German"
+	kbid="19"
+	region="de_CH"
+;;
 
-	french)
-		languagesetup -langspec 2
-	;;
+uk)
+	langspec="1"
+	lang="en"
+	keyboard="British"
+	kbid="2"
+	region="en_GB"
+;;
 
-	german)
-		languagesetup -langspec 3
-	;;
+us)
+	langspec="1"
+	lang="en"
+	keyboard="U.S."
+	kbid="0"
+	region="en_US"
+;;
 
-	simplifiedchinese)
-		languagesetup -langspec 4
-	;;
+sg)
+	langspec="1"
+	lang="en"
+	keyboard="U.S."
+	kbid="2"
+	region="en_SG"
+;;
 
-	traditionalchinese)
-		languagesetup -langspec 5
-	;;
+au)
+	langspec="1"
+	lang="en"
+	keyboard="Australian"
+	kbid="15"
+	region="en_AU"
+;;
 
-	japanese)
-		languagesetup -langspec 6
-	;;
+pl)
+	langspec="18"
+	lang="pl"
+	keyboard="Polish"
+	kbid="30762"
+	region="pl_PL"
+;;
 
-	spanish)
-		languagesetup -langspec 7
-	;;
-
-	italian)
-		languagesetup -langspec 8
-	;;
-
-	dutch)
-		languagesetup -langspec 9
-	;;
-
-	korean)
-		languagesetup -langspec 10
-	;;
-
-	portugesebrasil)
-		languagesetup -langspec 11
-	;;
-
-	portugese)
-		languagesetup -langspec 12
-	;;
-
-	danish)
-		languagesetup -langspec 13
-	;;
-
-	finnish)
-		languagesetup -langspec 14
-	;;
-
-	norwegian)
-		languagesetup -langspec 15
-	;;
-
-	swedish)
-		languagesetup -langspec 16
-	;;
-
-	russian)
-		languagesetup -langspec 17
-	;;
-
-	polish)
-		languagesetup -langspec 18
-	;;
-
-	turkish)
-		languagesetup -langspec 19
-	;;
-	
-	arabic)
-		languagesetup -langspec 20
-	;;
-	
-	thai)
-		languagesetup -langspec 21
-	;;
-	
-	czech)
-		languagesetup -langspec 22
-	;;
-	
-	hungarian)
-		languagesetup -langspec 23
-	;;
-	
-	catalan)
-		languagesetup -langspec 24
-	;;
-	
-	croatian)
-		languagesetup -langspec 25
-	;;
-	
-	greek)
-		languagesetup -langspec 26
-	;;
-	
-	hebrew)
-		languagesetup -langspec 27
-	;;
-	
-	romanian)
-		languagesetup -langspec 28
-	;;
-	
-	slovakian)
-		languagesetup -langspec 29
-	;;
-	
-	ukranian)
-		languagesetup -langspec 30
-	;;
-
-	indonesian)
-		languagesetup -langspec 31
-	;;
-
-	malay)
-		languagesetup -langspec 32
-	;;
-
-	vietnamese)
-		languagesetup -langspec 33
-	;;
-
-	mexicanspanish)
-		languagesetup -langspec 34
-	;;
-
-	*)
-		languagesetup -langspec 1
-	;;
+*)
+	#generic setting. allows user to configure manually later. japanese and other non roman languages require different settings.
+	langspec="1"
+	lang="en"
+	keyboard="U.S."
+	kbid="0"
+	region="en_US"
+;;
 
 esac
+
+# Set system language here. This mainly affects the loginwindow.
+languagesetup -langspec "$langspec"
+
+# Set region settings here for /Library
+/usr/libexec/Plistbuddy -c "Delete :AppleLocale" "/Library/Preferences/.GlobalPreferences.plist" &>/dev/null
+/usr/libexec/Plistbuddy -c "Add :AppleLocale string ${region}" "/Library/Preferences/.GlobalPreferences.plist" &>/dev/null
+/usr/libexec/Plistbuddy -c "Delete :Country" "/Library/Preferences/.GlobalPreferences.plist" &>/dev/null
+/usr/libexec/Plistbuddy -c "Add :Country string ${region:3:2}" "/Library/Preferences/.GlobalPreferences.plist" &>/dev/null
+
+# Set region settings here for User Template
+for uthome in "/System/Library/User Template"/*
+do
+	if [ -d "${uthome}"/Library/Preferences ]
+	then
+		/usr/libexec/Plistbuddy -c "Delete :AppleLocale" "${uthome}/Library/Preferences/.GlobalPreferences.plist" &>/dev/null
+		/usr/libexec/Plistbuddy -c "Add :AppleLocale string ${region}" "${uthome}/Library/Preferences/.GlobalPreferences.plist" &>/dev/null
+		/usr/libexec/Plistbuddy -c "Delete :Country" "${uthome}/Library/Preferences/.GlobalPreferences.plist" &>/dev/null
+		/usr/libexec/Plistbuddy -c "Add :Country string ${region:3:2}" "${uthome}/Library/Preferences/.GlobalPreferences.plist" &>/dev/null
+	fi
+done
+
+# Set user language for /Library
+/usr/libexec/Plistbuddy -c "Delete :AppleLanguages" "/Library/Preferences/com.apple.HIToolbox.plist" &>/dev/null
+if [ ${?} -eq 0 ]
+then
+	/usr/libexec/Plistbuddy -c "Add :AppleLanguages array" "/Library/Preferences/com.apple.HIToolbox.plist" &>/dev/null
+	/usr/libexec/Plistbuddy -c "Add :AppleLanguages:0 string '${lang}'" "/Library/Preferences/com.apple.HIToolbox.plist" &>/dev/null
+fi
+
+# Set keyboard settings here
+/usr/libexec/Plistbuddy -c "Delete :AppleCurrentKeyboardLayoutInputSourceID" "/Library/Preferences/com.apple.HIToolbox.plist" &>/dev/null
+
+if [ ${?} -eq 0 ]
+then
+	/usr/libexec/Plistbuddy -c "Add :AppleCurrentKeyboardLayoutInputSourceID string com.apple.keylayout.${kb}" "/Library/Preferences/com.apple.HIToolbox.plist" &>/dev/null
+fi
+
+for source in AppleDefaultAsciiInputSource AppleCurrentAsciiInputSource AppleCurrentInputSource AppleEnabledInputSources AppleSelectedInputSources
+do
+	/usr/libexec/Plistbuddy -c "Delete :${source}" "/Library/Preferences/com.apple.HIToolbox.plist"  &>/dev/null
+	if [ ${?} -eq 0 ]
+	then
+		/usr/libexec/Plistbuddy -c "Add :${source} array" "/Library/Preferences/com.apple.HIToolbox.plist" &>/dev/null
+		/usr/libexec/Plistbuddy -c "Add :${source}:0 dict" "/Library/Preferences/com.apple.HIToolbox.plist" &>/dev/null
+		/usr/libexec/Plistbuddy -c "Add :${source}:0:InputSourceKind string 'Keyboard Layout'" "/Library/Preferences/com.apple.HIToolbox.plist" &>/dev/null
+		/usr/libexec/Plistbuddy -c "Add :${source}:0:KeyboardLayout\ ID integer ${kbid}" "/Library/Preferences/com.apple.HIToolbox.plist" &>/dev/null
+		/usr/libexec/Plistbuddy -c "Add :${source}:0:KeyboardLayout\ Name string '${keyboard}'" "/Library/Preferences/com.apple.HIToolbox.plist" &>/dev/null
+	fi
+done
+
+# Force a recache of the plist since we made changes to it.
+killall cfprefsd
 
 # All done!
 
